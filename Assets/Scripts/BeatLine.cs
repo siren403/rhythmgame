5c560aefacfa2c75a7aa63770786c7bb648b7d05
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-05-24 17:17:50 +0900

    타이밍 보정 설계 중

---
+++ Assets/Scripts/BeatLine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatLine : MonoBehaviour {
+
+    public int BPM = 0;
+
+    private float BPS = 0.0f;
+
+    private List<float> BeatPos = new List<float>();
+
+    // Use this for initialization
+    void Start()
+    {
+        if(BPM <= 0)
+        {
+            return;
+        }
+
+        BPS = (float)BPM / 60.0f;
+
+        Debug.Log(string.Format("BPS : {0}", BPS));
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
