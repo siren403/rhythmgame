f375682cdd626f5df2faf50bc984fa300da01b2e
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-05-26 01:11:20 +0900

    - 홀인원 시작파트 구현

--- Assets/Scripts/BeatCounter.cs
+++ Assets/Scripts/BeatCounter.cs
@@ -9,6 +9,25 @@ using DG.Tweening;
 [RequireComponent(typeof(AudioSource))]
 public class BeatCounter : MonoBehaviour
 {
+    [System.Serializable]
+    public struct ActionInfo
+    {
+        public int index;
+        public float time;
+        public InputCode input;
+        public int soundCode;
+        public int actionCode;
+
+        public ActionInfo(int tIndex,float tTime)
+        {
+            index = tIndex;
+            time = tTime;
+            actionCode = 0;
+            soundCode = -1;
+            input = InputCode.None;
+        }
+    }
+
     public enum InputResult
     {
         None, 
@@ -25,13 +44,15 @@ public class BeatCounter : MonoBehaviour
     public float BPM = 129;
     private float BPS = 0.0f;
 
+    public float StartBeatOffset = 0.0f;
+
     private float mCurrentTime = 0;
 
     private bool mIsPlaying = false;
     public bool mIsMusicPlay = false;
 
     private AudioSource mAudioSource = null;
-    public AudioClip mInputSound = null;
+    public List<AudioClip> SEList = new List<AudioClip>();
 
     private float CurrentTrackTime
     {
@@ -47,7 +68,7 @@ public class BeatCounter : MonoBehaviour
                 tTime = mCurrentTime;
             }
 
-            tTime -= BPS * 2.25f;
+            tTime += BPS * StartBeatOffset;
 
             if(tTime < 0)
             {
@@ -66,59 +87,53 @@ public class BeatCounter : MonoBehaviour
         }
     }
 
-    private float mPrevBeatCount = 0.0f;
-
     public Transform[] mCubeArray = null;
 
-    private byte[] mBeatData = new byte[] 
-    {
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-    };
-    private int mBeatIndex
-    {
-        get
-        {
-            if(mBeatProgress > 1)
-            {
-                int index = Mathf.RoundToInt(mBeatProgress) - 1;
-                return index - ((int)(index / mBeatData.Length)) * mBeatData.Length;
-            }
-            return 0;
-        }
-    }
+    public List<ActionInfo> PushActionInfo = new List<ActionInfo>();
+    public List<ActionInfo> mActionInfoList = new List<ActionInfo>();
+   
+    //private int mBeatIndex
+    //{
+    //    get
+    //    {
+    //        if(mBeatProgress > 1)
+    //        {
+    //            int index = Mathf.RoundToInt(mBeatProgress) - 1;
+    //            return index - ((int)(index / mActionInfoList.Count)) * mActionInfoList.Count;
+    //        }
+    //        return 0;
+    //    }
+    //}
 
     private float mPerfectRatio = 0.2f;
+    private int mPrevRoundProgress = 0;
+    private int mActionIndex = 0;
 
-
-
+    public GameObject PFBall = null;
+    public Transform BallStartPoint = null;
+    public Transform BallEndPoint = null;
+    private GameObject CurrentBall = null;
     // Use this for initialization
     void Start()
     {
         mAudioSource = GetComponent<AudioSource>();
-        BPS = 60.0f / BPM;
+        BPS = 60.0f / (float)BPM;
+        Debug.Log("BPS : " + BPS);
+        for (int i = 0; i < mAudioSource.clip.length / BPS; i++)
+        {
+            mActionInfoList.Add(new ActionInfo(i, i * BPS));
+        }
+        Debug.Log("ActionInfo Count : " + mActionInfoList.Count);
+
+        foreach(var info in PushActionInfo)
+        {
+            var tBaseInfo = mActionInfoList[info.index];
+            tBaseInfo.input = info.input;
+            tBaseInfo.soundCode = info.soundCode;
+            tBaseInfo.actionCode = info.actionCode;
+            mActionInfoList[info.index] = tBaseInfo;
+        }
+
     }
 
     // Update is called once per frame
@@ -138,21 +153,44 @@ public class BeatCounter : MonoBehaviour
             {
                 Stop();
             }
-            float tBeatCount = mBeatProgress;
-            int tCount = Mathf.RoundToInt(tBeatCount);
+
+            if (mActionInfoList[mActionIndex].time - CurrentTrackTime <= 0.001f)
+            {
+                if (mActionInfoList[mActionIndex].soundCode != -1)
+                {
+                    mAudioSource.PlayOneShot(SEList[mActionInfoList[mActionIndex].soundCode]);
+                    
+
+                }
+                if(mActionInfoList[mActionIndex].actionCode != 0)
+                {
+                    if (CurrentBall == null)
+                    {
+                        CurrentBall = Instantiate(PFBall, BallStartPoint.position, Quaternion.identity);
+                    }
+
+                    CurrentBall.transform.position = BallStartPoint.position;
+                    CurrentBall.SetActive(true);
+                    CurrentBall.transform.DOJump(BallEndPoint.position, 2, 1, BPS)
+                        .SetEase(Ease.Linear);
+                }
+
+                mActionIndex++;
+            }
 
             InputCode tInputCode = InputCode.None;
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log(mBeatIndex);
                 tInputCode = InputCode.SpaceDown;
+                Debug.Log(mActionIndex);
+
             }
             else if(Input.GetKeyUp(KeyCode.Space))
             {
                 tInputCode = InputCode.SpaceUp;
             }
 
-            InputResult tResult = CheckInputTiming(tInputCode, tCount, tBeatCount);
+            InputResult tResult = CheckInputTiming(tInputCode);
             switch (tResult)
             {
                 case InputResult.Fast:
@@ -169,18 +207,8 @@ public class BeatCounter : MonoBehaviour
                     break;
             }
 
-            //if ((int)mPrevBeatCount != tCount)
-            //{
-            //    mPrevBeatCount = tCount;
 
-            //    int tCubeIndex = (int)Mathf.Repeat(mBeatIndex, 8);
-            //    if (tCubeIndex < mCubeArray.Length)
-            //    {
-            //        mCubeArray[tCubeIndex].localScale = Vector3.one * 0.6f;
-            //        mCubeArray[tCubeIndex].DOScale(Vector3.one * 0.5f, 0.0f).SetDelay(BPS);
-            //    }
-
-            //}
+           
 
             if (mIsMusicPlay == false)
             {
@@ -195,25 +223,29 @@ public class BeatCounter : MonoBehaviour
     /// 입력에 따른 판정을 실시한다
     /// </summary>
     /// <param name="tInputCode">입력 타입</param>
-    /// <param name="tCount">현재 비트</param>
-    /// <param name="tCurrentTiming">입력 시 비트 타이밍</param>
+    /// <param name="tRoundProgress">현재 비트</param>
+    /// <param name="tBeatProgress">입력 시 비트 타이밍</param>
     /// <returns>판정 결과</returns>
-    private InputResult CheckInputTiming(InputCode tInputCode, int tCount, float tCurrentTiming)
+    private InputResult CheckInputTiming(InputCode tInputCode)
     {
+
+        int tRoundProgress = Mathf.RoundToInt(mBeatProgress);
+
         InputResult tResult = InputResult.Fail;
 
-        if (mBeatData[mBeatIndex] != 0)
+        if (mActionInfoList[mActionIndex].input != InputCode.None)
         {
-            if ((tInputCode == InputCode.SpaceDown && mBeatData[mBeatIndex] == 1) ||
-                (tInputCode == InputCode.SpaceUp && mBeatData[mBeatIndex] == 2))
-            {
 
-                if (tCount - mPerfectRatio > tCurrentTiming)
+            //((tInputCode == InputCode.SpaceDown && mBeatData[mBeatIndex] == 1) ||
+            //(tInputCode == InputCode.SpaceUp && mBeatData[mBeatIndex] == 2))
+            if (mActionInfoList[mActionIndex].input == tInputCode)
+            {
+                if (tRoundProgress - mPerfectRatio > mBeatProgress)
                 {
                     Debug.Log("Too fast");
                     tResult = InputResult.Fast;
                 }
-                else if (tCount + mPerfectRatio < tCurrentTiming)
+                else if (tRoundProgress + mPerfectRatio < mBeatProgress)
                 {
                     Debug.Log("Too Late");
                     tResult = InputResult.Late;
@@ -222,11 +254,14 @@ public class BeatCounter : MonoBehaviour
                 {
                     Debug.Log("Perfect!!");
                     tResult = InputResult.Perfect;
+                    CurrentBall.SetActive(false);
                 }
-
-                Debug.Log(tResult.ToString() + " / " + string.Format("{0} [{1}] {2}", tCount - mPerfectRatio, tCurrentTiming, tCount + mPerfectRatio));
+                Debug.Log(tResult.ToString() + " / " + string.Format("{0} [{1}] {2}",
+                    tRoundProgress - mPerfectRatio,
+                    mBeatProgress,
+                    tRoundProgress + mPerfectRatio));
             }
-
+            
         }
 
         return tResult;
@@ -234,7 +269,7 @@ public class BeatCounter : MonoBehaviour
 
     private void ChangeCubeColor(Color tColor)
     {
-        int tCubeIndex = (int)Mathf.Repeat(mBeatIndex, 8);
+        int tCubeIndex = (int)Mathf.Repeat(mActionIndex, 8);
         var mat = mCubeArray[tCubeIndex].GetComponent<Renderer>().material;
         mat.color = tColor;
         mat.DOColor(Color.white, 0.0f).SetDelay(BPS);
