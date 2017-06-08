215cff0daea8db273cce219c18759a138d7325b5
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-06-08 16:54:45 +0900

    - 3박자 모션

--- Assets/Scripts/Stage/Theme/CHoleInOne.cs
+++ Assets/Scripts/Stage/Theme/CHoleInOne.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UniRx.Toolkit;
 using System;
 using DG.Tweening;
+
 
 public class CHoleInOne : CThemeBase
 {
     private const int BEAT_LEVEL_1 = 39;
     private const int BEAT_LEVEL_2 = 76;
+    private const string KEY_TRIGGER_BEAT = "TrigBeat";
+    private const string KEY_TRIGGER_SHORT = "TrigShort";
 
     protected override IPresenter[] Children
     {
@@ -31,6 +35,10 @@ public class CHoleInOne : CThemeBase
 
     public Animator AnimMonkey = null;
     public Animator AnimMandrill = null;
+    public Animator AnimGolfer = null;
+
+    private Queue<GameObject> mBallPool = new Queue<GameObject>();
+    private Queue<GameObject> mActiveBallPool = new Queue<GameObject>();
 
     public Renderer BeatPanel = null;
     public Renderer CheckTimingPanel = null;
@@ -38,27 +46,19 @@ public class CHoleInOne : CThemeBase
     public Transform BallStartPoint = null;
     public Transform BallEndPoint = null;
     public Transform BallPerpectPoint = null;
-    private GameObject _CurrentBall = null;
-    private GameObject CurrentBall
-    {
-        get
-        {
-            if (_CurrentBall == null)
-            {
-                _CurrentBall = Instantiate(PFBall, BallStartPoint.position, Quaternion.identity);
-                _CurrentBall.transform.SetParent(this.transform);
-            }
-            return _CurrentBall;
-        }
-    }
 
 
     private Dictionary<string, Action<CSequencePlayer, CSequenceData>> mActionList 
         = new Dictionary<string, Action<CSequencePlayer, CSequenceData>>();
 
+
     protected override void BeforeInitialize()
     {
-        mActionList[CHoleInOneActionCode.SEMONKEY] = (tSeqPlayer, tSeqData) => mAudioSource.PlayOneShot(SEMonkey);
+        mActionList[CHoleInOneActionCode.SEMONKEY] = (tSeqPlayer, tSeqData) =>
+        {
+            mAudioSource.PlayOneShot(SEMonkey);
+            AnimMonkey.SetTrigger(KEY_TRIGGER_SHORT);
+        };
         mActionList[CHoleInOneActionCode.SEMANDRIL] = (tSeqPlayer, tSeqData) => mAudioSource.PlayOneShot(SEMandril);
 
         mActionList[CHoleInOneActionCode.THROWBALL] = ThrowBall;
@@ -73,7 +73,7 @@ public class CHoleInOne : CThemeBase
     }
     public override void OnEveryBeat(CSequencePlayer tSeqPlayer, CSequenceData tData)
     {
-        AnimMonkey.SetTrigger("TrigBeat");
+        AnimMonkey.SetTrigger(KEY_TRIGGER_BEAT);
         switch ((int)tSeqPlayer.BeatProgress)
         {
             case BEAT_LEVEL_1:
@@ -84,7 +84,8 @@ public class CHoleInOne : CThemeBase
                 break;
         }
 
-        AnimMandrill.SetTrigger("TrigBeat");
+        AnimMandrill.SetTrigger(KEY_TRIGGER_BEAT);
+        AnimGolfer.SetTrigger(KEY_TRIGGER_BEAT);
         if (mActionList.ContainsKey(tData.ActionCode))
         {
             mActionList[tData.ActionCode].Invoke(tSeqPlayer, tData);
@@ -104,8 +105,18 @@ public class CHoleInOne : CThemeBase
                 break;
             case InputResult.Perfect:
                 CheckTimingPanel.material.DOColor(Color.green, tSeqPlayer.BPS * 0.8f).From();
-                CurrentBall.transform.DOJump(BallPerpectPoint.position, 2, 1, tSeqPlayer.BPS)
-                   .OnComplete(() => CurrentBall.SetActive(false));
+
+                if (mActiveBallPool.Count > 0)
+                {
+                    GameObject tBall = mActiveBallPool.Dequeue();
+                    tBall.transform.DOScale(0, tSeqPlayer.BPS * 2.0f).SetEase(Ease.OutQuad);
+                    tBall.transform.DOJump(BallPerpectPoint.position, 4, 1, tSeqPlayer.BPS * 2.0f)
+                       .SetEase(Ease.OutQuad)
+                       .OnComplete(() =>
+                       {
+                           ReturnPoolBall(tBall);
+                       });
+                }
                 break;
             case InputResult.Late:
                 CheckTimingPanel.material.DOColor(Color.blue, tSeqPlayer.BPS * 0.8f).From();
@@ -116,26 +127,51 @@ public class CHoleInOne : CThemeBase
 
     private void ThrowBall(CSequencePlayer tSeqPlayer, CSequenceData tData)
     {
-        CurrentBall.transform.position = BallStartPoint.position;
-        CurrentBall.SetActive(true);
-        CurrentBall.transform.DOJump(BallEndPoint.position, 2, 1, tSeqPlayer.BPS)
+        AnimMonkey.SetTrigger(KEY_TRIGGER_SHORT);
+        GameObject tBall = RentBall();
+        tBall.transform.position = BallStartPoint.position;
+        tBall.transform.DOJump(BallEndPoint.position, 2, 1, tSeqPlayer.BPS)
             .SetEase(Ease.Linear);
     }
+
     private void ThrowStraightBall(CSequencePlayer tSeqPlayer,CSequenceData tData)
     {
-        CurrentBall.transform.position = BallStartPoint.position;
-        CurrentBall.SetActive(true);
-        CurrentBall.transform.DOMove(BallEndPoint.position, tSeqPlayer.BPS * 0.1f)
+        GameObject tBall = RentBall();
+        tBall.transform.position = BallStartPoint.position;
+        tBall.transform.DOMove(BallEndPoint.position, tSeqPlayer.BPS * 0.1f)
            .SetEase(Ease.Linear);
-
     }
     private void MonkeyShort(CSequencePlayer tSeqPlayer, CSequenceData tData)
     {
         mAudioSource.PlayOneShot(SEMonkeyShort);
-        DOTween.Kill(CurrentBall.transform);
-        CurrentBall.transform.position = BallStartPoint.position;
-        CurrentBall.SetActive(true);
-        CurrentBall.transform.DOJump(BallEndPoint.position, 2, 1, tSeqPlayer.BPS)
+        GameObject tBall = RentBall();
+        tBall.transform.position = BallStartPoint.position;
+        tBall.transform.DOJump(BallEndPoint.position, 2, 1, tSeqPlayer.BPS)
             .SetEase(Ease.Linear);
+    }
+
+    private GameObject RentBall()
+    {
+        GameObject tBall = null;
+
+        if (mBallPool.Count > 0)
+        {
+            tBall = mBallPool.Dequeue();
+        }
+        else
+        {
+            tBall = Instantiate(PFBall, BallStartPoint.position, Quaternion.identity);
+            tBall.transform.SetParent(this.transform);
+        }
+
+        tBall.gameObject.SetActive(true);
+        mActiveBallPool.Enqueue(tBall);
+        return tBall;
+    }
+    private void ReturnPoolBall(GameObject tGameObject)
+    {
+        tGameObject.SetActive(false);
+        tGameObject.transform.localScale = Vector3.one;
+        mBallPool.Enqueue(tGameObject);
     }
 }
